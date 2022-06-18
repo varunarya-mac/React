9cdfe61967963b6f9594bb7dbbd446d3012cbdb4
Varun Arya <varun.arya@192.168.1.115>
2022-06-18 20:42:34 +0100

    added react dom portal logic

--- Form_Apps/basic_user_form/src/components/shared/PopUp.js
+++ Form_Apps/basic_user_form/src/components/shared/PopUp.js
@@ -1,27 +1,29 @@
 import styles from "./PopUp.module.css";
 import Card from "./Card";
 import Button from "./Button";
+import ReactDOM from 'react-dom'
+import { Fragment } from "react";
+
+const PopupUI = (props) => {return <Card className={styles.modal}>
+  <header className={styles.header}>
+    <h2>Error:</h2>
+  </header>
+  <div className={styles.content}>
+    <p>{props.content}</p>
+  </div>
+  <footer className={styles.actions}>
+    <Button  title="Ok" action={props.handleClose}></Button>
+  </footer>
+</Card>;
+}
+
 
 const Popup = (props) => {
   return (
-    <Card className={styles.modal}>
-      <header className={styles.header}>
-        <h2>Error:</h2>
-      </header>
-      <div className={styles.content}>
-        <p>{props.content}</p>
-      </div>
-      <footer className={styles.actions}>
-        <Button  title="Ok" action={props.handleClose}></Button>
-      </footer>
-    </Card>
-
-    // <div className={styles.popup}>
-    //   <div className={styles.box_}>
-    //     <span className={styles.close_icon} onClick={props.handleClose}>x</span>
-    //     {props.content}
-    //   </div>
-    // </div>
+    <Fragment>
+      {ReactDOM.createPortal(<PopupUI handleClose={props.handleClose} />, document.getElementById('popup'))}
+    </Fragment>
+    
   );
 };
 
